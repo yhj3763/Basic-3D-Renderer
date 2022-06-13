2ce376bf18245bb2fb6b330de4d729048a287e91
YHJ <yaj3763@naver.com>
2022-06-12 23:23:09 -0700

    ImGui

--- shader/simple.vs
+++ shader/simple.vs
@@ -3,13 +3,14 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
 out vec2 TexCoord;
+uniform mat4 transform;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection*view*model* vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position =  projection * view *transform* model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
  
     TexCoord = aTexCoord;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h""
@@ -15,6 +18,9 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void OnKeyEvent(GLFWwindow* window,int key, int scancode, int action, int mods);
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset);
+void OnCharEvent(GLFWwindow* window, unsigned int ch);
 
 
 int main()
@@ -32,6 +38,8 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
+ 
+
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         SPDLOG_ERROR("Faild to initialize GLAD");
@@ -39,6 +47,7 @@ int main()
     }
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+ 
 
     glEnable(GL_DEPTH_TEST);
 
@@ -169,13 +178,50 @@ int main()
     glUniform1i(glGetUniformLocation(ourShader.ID, "texture2"),1);
  
     
+    auto imguiContext = ImGui::CreateContext();
+    // ImGuiIO& io = ImGui::GetIO(); (void)io;    
+    // ImGui::StyleColorsDark();
+    // ImGui_ImplGlfw_InitForOpenGL(window, true);
+    // ImGui_ImplOpenGL3_Init();
+    ImGui::SetCurrentContext(imguiContext);
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplOpenGL3_CreateFontsTexture();
+    ImGui_ImplOpenGL3_CreateDeviceObjects();
+
+
+    
+    // glm::mat4 trans = glm::mat4(1.0f);
+    // glm::vec3 vtrans = glm::vec3(0.5f,0.5f, 0.5f);
+    // trans = glm::scale(trans, vtrans);
+
+ 
+    float angle = 50.0f;
+    glm::vec3 vmodel =glm::vec3(0.5f,1.0f,0.0f);
+    glm::vec3 Vview = glm::vec3(0.0f, 1.0f, -3.0f);
+    glm::vec3 vtrans = glm::vec3(0.5f,0.5f, 5.0f);
 
     while(!glfwWindowShouldClose(window)){
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
+        if(ImGui::Begin("my first ImGui window")){
+            ImGui::SliderFloat3("Vtrans", glm::value_ptr(vtrans),-10.0f, 10.0f );
+            ImGui::SliderFloat3("Vview", glm::value_ptr(Vview),-30.0f, 30.0f );
+             
+        }
+        ImGui::End();
+       
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+     
+
+
+    
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -185,24 +231,7 @@ int main()
 
         ourShader.use();
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime()*(glm::radians(50.0f)), glm::vec3(0.5f,1.0f,0.0f));
-
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
-
-        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE,  glm::value_ptr(view));
-
-        ourShader.setMat4("projection", projection);
-
-
+   
         glBindVertexArray(VAO);
         for (unsigned int i = 0; i < 10; i++)
         {
@@ -221,11 +250,47 @@ int main()
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+       
 
+      
+
+    
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::scale(trans, vtrans);
+
+            
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime()*(glm::radians(angle)), vmodel);
+
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, Vview);
+
+            
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+
+    
+        ourShader.use();
+    
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "view"), 1, GL_FALSE,  glm::value_ptr(view));
+
+        ourShader.setMat4("projection", projection);
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
  
+    ImGui_ImplOpenGL3_DestroyFontsTexture();
+    ImGui_ImplOpenGL3_DestroyDeviceObjects();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext(imguiContext);
+
     glfwTerminate();
     return 0;
 }
@@ -239,4 +304,28 @@ void processInput(GLFWwindow *window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
     glViewport(0,0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset) {
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
+}
+
+void OnCharEvent(GLFWwindow* window, unsigned int ch) {
+    ImGui_ImplGlfw_CharCallback(window, ch);
 }
